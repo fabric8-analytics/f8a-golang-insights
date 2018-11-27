518d7612c92467a67cda0264e14971ecbc709973
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-11-27 12:31:19 +0100

    Fixed docstrings

--- insights_engine/rest_api.py
+++ insights_engine/rest_api.py
@@ -10,18 +10,18 @@ logger = daiquiri.getLogger(__name__)
 
 
 def companion_recommendation(stack):
-    """The main companion recommendation endpoint that serves incoming requests."""
+    """Endpoint for the  main companion recommendation endpoint that serves incoming requests."""
     # TODO
     return json.dumps({}), 200
 
 
 def liveness():
-    """The liveness probe."""
+    """Endpoint with the implementation of hhe liveness probe."""
     return NoContent, 200
 
 
 def readiness():
-    """The readiness probe."""
+    """Endpoint with the implementation the readiness probe."""
     logger.debug("Service up and ready to serve requests.")
     return NoContent, 200
 
